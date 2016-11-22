db0f3d348f933b54c760c1c5fb7e273aeca0a78c
Henry `Parra <Henry `Parra>
2016-11-22 09:23:50 -0500

    sifnsahfbsjfb231254

---
+++ storage/framework/views/80fdb29b4618eaf2fdb6f2269f30ba40345e8ac8.php
@@ -0,0 +1,104 @@
+<?php $__env->startSection('content'); ?>
+
+</div>
+
+
+<body>
+<div class="image-container set-full-height" style="background-image: url('img/office1.jpeg')">
+
+    <!--  Slider  -->
+    <a href="http://tecnoparque.sena.edu.co/" class="made-with-mk">
+        <div class="material-icons">copyright</div>
+        <div class="made-with">Hecho Para <strong>Tecnoparque</strong></div>
+    </a>
+
+    <!--   Big container   -->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2 contentiniciodiv">
+                <div class="wizard-container">
+                    <div class="card wizard-card" data-color="green" id="wizardProfile">
+                        <div class="wizard-header">
+                            <h3 class="wizard-title">
+                                Red Tecnoparque Colombia
+                            </h3>
+                            <h5>Regional Caldas</h5>
+                        </div>
+                        <div class="wizard-navigation">
+                            <ul>
+                                <li class="active"><a href="#proyectos" data-toggle="tab">Proyectos</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="tab-content col-sm-6 col-sm-offset-1 iniciodiv">
+                            <?php $query = DB::table("proyectos")->where('estadosdeproyectos_id', '<>', '1')->where('estadosdeproyectos_id', '<>', '2')->get(); ?>
+                            <?php $__currentLoopData = $query; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $row): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
+
+                        <div class="tab-content col-sm-6 col-sm-offset-3">
+                            <?php $__currentLoopData = $proyectos; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $row): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
+                            <div class="card-container">
+                                <div class="card">
+                                    <div class="front">
+                                        <div class="cover">
+                                            
+                                            <img src="<?php echo e($row->imagen); ?>"/>
+                                        </div>
+                                        <br><br><br><br><br>
+                                        <div class="content">
+                                            <div class="main">
+                                                <h3 class="name"><?php echo e($row->nombrep); ?></h3>
+                                                <p class="text-justify"><?php echo e($row->descripcion); ?></p>
+                                            </div>
+                                        </div>
+                                    </div> <!-- end front panel -->
+                                    <div class="back">
+                                        <div class="header">
+                                            <h5 class="text-center">Sector Enfocado</h5>
+                                            <p class="text-center"><?php echo e($row->sectorenfocado); ?></p>
+                                        </div>
+                                        <div class="content">
+                                            <div class="main">
+                                                <h4 class="text-center">Empresa</h4>
+                                                <p class="text-center"><?php echo e($row->empresa); ?></p>
+
+                                                <div class="stats-container">
+                                                    <h4 class="text-center">Estado</h4>
+                                                    <p class="text-center">
+                                                        <?php $estados = DB::table("estadosdeproyectos")->get(); ?>
+                                                        <?php $actualizarestados = DB::table("proyectos")->where("estadosdeproyectos_id", $row->estadosdeproyectos_id)->get(); ?>
+                                                        <?php $__currentLoopData = $estados; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $estado): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
+                                                            <?php if($estado->id == $row->estadosdeproyectos_id): ?>
+                                                                <?php echo e($estado->estado); ?>
+
+                                                            <?php endif; ?>
+                                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="footer">
+                                            <div class="social-links text-center">
+                                                <a href="" class="facebook"><i class="fa fa-facebook fa-fw"></i></a>
+                                                <a href="" class="google"><i class="fa fa-google-plus fa-fw"></i></a>
+                                                <a href="" class="twitter"><i class="fa fa-twitter fa-fw"></i></a>
+                                            </div>
+                                        </div>
+                                    </div> <!-- end back panel -->
+                                </div> <!-- end card -->
+                            </div> <!-- end card-container -->
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+<?php $__env->stopSection(); ?>
+
+
+
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
